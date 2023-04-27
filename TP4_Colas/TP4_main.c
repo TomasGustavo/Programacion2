2aca15cf503d198607c335fc5b0833a7ce1a361e
Valentina Lopez <valentinalopezh98@gmail.com>
2023-04-27 11:53:25 -0300

    add main

---
+++ TP4_Colas/TP4_main.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "TP4_colas.h"
+#include "colas.h"
+#include "tipo_elemento.h"
+
+void menu_principal()
+{
+    printf("\n");
+    printf(ANSI_BLUE "  ============================================================================\n");
+    printf(" |                                 TP3 PILAS                                 |\n");
+    printf("  ============================================================================\n");
+    printf("\n");
+    printf("  2   Operaciones con colas\n");
+    printf("  3   Colas iguales\n");
+    printf("  4   Elementos no repetidos\n");
+    printf("  5   Divisores\n");
+    printf("  6   Lista con valores comunes\n");
+    printf("  7   Cola de clientes\n");
+    printf("\n");
+    printf("  0   Salir\n");
+    printf("\n");
+    printf(" ------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("  Por favor seleccione una opción: " ANSI_YELLOW);
+}
+
+void menu_punto2()
+{
+    printf("\n");
+    printf(ANSI_BLUE "  ============================================================================\n");
+    printf(" |                         2   Operaciones con pilas                         |\n");
+    printf("  ============================================================================\n");
+    printf("\n");
+    printf("  1   Buscar elemento\n");
+    printf("  2   Insertar elemento\n");
+    printf("  3   Eliminar elemento\n");
+    printf("  4   Contar los elementos\n");
+    printf("  5   Realizar una copia\n");
+    printf("  6   Invertir\n");
+    printf("\n");
+    printf("  0   Salir\n");
+    printf("\n");
+    printf(" ------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("  Por favor seleccione una opción: " ANSI_YELLOW);
+}
+
+int main()
+{
+    bool salir1 = false;
+    bool salir = false;
+    int opcion;
+    while (!salir)
+    {
+        menu_principal();
+        scanf("%i", &opcion);
+        while (getchar() != '\n')
+            ;
+        while (opcion < 0 || opcion > 8 || opcion == 1)
+        {
+            printf(ANSI_RED "Opción incorrecta\n" ANSI_RESET);
+            printf(ANSI_BLUE "  Por favor seleccione una opción: " ANSI_YELLOW);
+            scanf("%i", &opcion);
+            while (getchar() != '\n')
+                ;
+        }
+        switch (opcion)
+        {
+        case 2:
+            while (!salir1)
+            {
+                menu_punto2();
+                scanf("%i", &opcion);
+                while (opcion < 0 || opcion > 6)
+                {
+                    printf(ANSI_RED "Opción incorrecta\n" ANSI_RESET);
+                    printf(ANSI_BLUE "  Por favor seleccione una opción: " ANSI_YELLOW);
+                    while (getchar() != '\n')
+                        ;
+                    scanf("%i", &opcion);
+                }
+                switch (opcion)
+                {
+                case 1:
+                    main_buscar_clave();
+                    getch();
+                    break;
+                case 2:
+                    main_insertar_en_pila();
+                    getch();
+                    break;
+                case 3:
+                    main_eliminar_por_clave();
+                    getch();
+                    break;
+                case 4:
+                    main_intercambiar();
+                    getch();
+                    break;
+                case 5:
+                    main_duplicar_pila();
+                    getch();
+                    break;
+                case 6:
+                    main_contador();
+                    getch();
+                    break;
+                case 0:
+                    salir1 = true;
+                }
+            }
+            break;
+            menu_principal();
+        case 3:
+            main_compara_pilas();
+            getch();
+            break;
+        case 4:
+            main_conversor();
+            getch();
+            break;
+        case 5:
+            main_invertida();
+            getch();
+            break;
+        case 6:
+            main_eliminar_ocurrencias();
+            getch();
+            break;
+        case 7:
+            main_elemento_en_comun();
+            getch();
+            break;
+        case 8:
+            main_contador_valores();
+            getch();
+            break;
+        case 0:
+            salir = true;
+            break;
+        }
+    }
+    return 0;
+}
